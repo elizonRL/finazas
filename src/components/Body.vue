76297eaf85bbc25db1e0e6790d00ec33effd3f79
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-03-29 15:27:19 -0400

    Actualiza estilos y funcionalidad de la página de finanzas personales

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,38 +1,63 @@
 <script setup>
+import Moneybag from './icons/Moneybag.vue';
 import { ref } from 'vue';
 const ingresos = ref('');
-const data = [
-    {ingreso: ""},
-]
+const data = ref([]);
 
 const ingresosP = () => {
-    data.push({ingreso: ingresos.value})
+    data.value.push({ ingreso: ingresos.value })
     ingresos.value = ''
-   
+    
+
 }
 
 
 </script>
 <template>
-    <h1>Finazas personales</h1>
-    <span>Crea tu control de finazas</span>
-
-    <form >
-        <div>
-            <label for="Ingresos">Ingresos</label>
-            <input type="text" id="concepto" v-model="ingresos"/>
+    <section>
+        <div class="header_finanza">
+            <Moneybag />
+            <h1>Finazas personales</h1>
         </div>
-        <button @submit.prevent="ingresosP">Agregar</button>
-    </form>
-    <div v-for="datas in data ">
-        <p>{{datas.ingreso}}</p>
-    </div>
+        <article>
+            <form @submit.prevent="ingresosP">
+                <div>
+                    <label for="Ingresos">Ingresos</label>
+                    <input type="text" id="concepto" v-model="ingresos" />
+                </div>
+                <button>Agregar</button>
+            </form>
+
+        </article>
+        <div  v-if="data.length !== 0">
+            <h2>Mis ingresos</h2>
+            <p v-for="datas in data ">{{ datas.ingreso }}</p>
+        </div>
+    </section>
 </template>
 <style scoped>
-form {
-    margin: 2rem auto;
-
+article {
+    margin:  auto;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    width: 50%;
+    justify-content: center;
+    text-align: center;
+    box-shadow: 0, 5px, 20px rgba(0, 0, 2, 0.2);
+    background: #fff;
 }
+
+form {
+    margin: 1rem auto;
+}
+.header_finanza {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem auto;
+}
+
 button {
     padding: 0.5rem 1rem;
     background-color: #333;
@@ -41,16 +66,20 @@ button {
     border-radius: 0.25rem;
     cursor: pointer;
 }
+
 button:hover {
     background-color: var(--color-secondary);
 }
+
 input {
     width: 100%;
-    padding: 0.9rem;
-    margin: 0.5rem 0;
-    border-bottom: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px;
+    border: 2px solid rgba(103, 224, 158, 0.5);
     border-radius: 0.25rem;
+    background: transparent;
 }
+
 label {
     font-weight: bold;
     display: block;
