577ddeb0556ae384b3081921b2628ad631ac5937
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-04-05 22:42:29 -0400

    Refactorizar el componente FromEditIngresos.vue y corregir errores de formato

--- src/components/FromEditIngresos.vue
+++ src/components/FromEditIngresos.vue
@@ -1,5 +1,5 @@
 <template>
-     <div class="conten">
+    <div class="conten">
         <div class="form-group">
             <label for="month">Mes</label>
             <input type="month" id="month" v-model="data.month" />
@@ -8,11 +8,10 @@
             <label for="ingreso">Ingreso</label>
             <input type="text" id="ingreso" v-model="data.ingreso" />
         </div>
-     </div>
+    </div>
 </template>
 <script setup>
-import { reactive } from 'vue';
- 
+
 defineProps({
     data: Object
 })
@@ -25,27 +24,30 @@ defineProps({
     flex-direction: column;
     gap: 1rem;
 }
+
 .form-group {
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
 }
+
 input {
     width: 100%;
     padding: 1rem;
     border: 2px solid rgba(103, 224, 158, 0.5);
     border-radius: 0.5rem;
 }
+
 input[type="month"] {
-  width: 100%;
-  border: none;
+    width: 100%;
+    border: none;
 }
 
 input[type="month"]:focus {
-  outline: none;
-}
-input[type="text"]:focus {
-  outline: none;
+    outline: none;
 }
 
+input[type="text"]:focus {
+    outline: none;
+}
 </style>
